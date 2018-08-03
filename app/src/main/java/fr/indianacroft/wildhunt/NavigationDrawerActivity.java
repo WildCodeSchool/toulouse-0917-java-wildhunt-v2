b5a5a32d532aa9019ad85a12ba082f5c45143898
bastienwcs <bastien@wildcodeschool.fr>
2018-08-03 20:43:51 +0200

    fix

--- app/src/main/java/fr/indianacroft/wildhunt/NavigationDrawerActivity.java
+++ app/src/main/java/fr/indianacroft/wildhunt/NavigationDrawerActivity.java
@@ -9,7 +9,6 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ScrollView;
@@ -49,7 +48,6 @@ public class NavigationDrawerActivity extends AppCompatActivity
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
     }
-
 
     @Override
     protected void onResume() {
